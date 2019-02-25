d2356e2d7149f7e5e04a5aef5de32a16fcac58d1
Codegnosis <paul@codegnosis.co.uk>
2019-02-25 10:32:25 +0000

    abi script - ABI and deployed address functionality

--- abi.js
+++ abi.js
@@ -2,10 +2,27 @@ const fs = require('fs');
 
 const ContractJsonPath = './build/contracts/WorkchainRoot.json'
 
-
 function main() {
+    let args = process.argv.slice(2);
     let contract = JSON.parse(fs.readFileSync(ContractJsonPath, 'utf8'));
+    
+    switch (args[0]) {
+       case 'abi':
+       default:
+           abi(contract);
+           break;
+       case 'addr':
+           contract_address(contract, args[1]);
+           break;
+    }
+}
+
+function abi(contract) {
     console.log(JSON.stringify(contract.abi));
+}
+
+function contract_address(contract, networkId) {
+    console.log(contract.networks[networkId].address);
 }
 
 main();

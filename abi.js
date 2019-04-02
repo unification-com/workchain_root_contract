84636f2bf227ac1f63672e7f3970a2ad79918b61
Codegnosis <paul@codegnosis.co.uk>
2019-04-02 12:11:14 +0100

    abi tool - allow contract type to be passed as arg. Tweaks & improvements

--- abi.js
+++ abi.js
@@ -3,21 +3,37 @@ const fs = require('fs');
 
 let ContractJsonPath = './build/contracts/WRKChainRoot.json'
 
-if( process.env.WRKCHAIN_CONTRACT == 'slim') {
-    ContractJsonPath = './build/contracts/WRKChainRootSlim.json'
-}
-
 function main() {
     let args = process.argv.slice(2);
+    let cmd = args[0];
+    let network_id = args[1];
+    let contract_type = args[2];
+
+    if(cmd === null || cmd === undefined) {
+        console.log("Error: No command specified. Requires 'addr' or 'abi'");
+        help();
+        return;
+    }
+
+    if(network_id === null || network_id === undefined) {
+        console.log("Error: No Mainchain Network ID.");
+        help();
+        return;
+    }
+
+    if (contract_type == 'slim' || process.env.WRKCHAIN_CONTRACT == 'slim') {
+        ContractJsonPath = './build/contracts/WRKChainRootSlim.json';
+    }
+
     let contract = JSON.parse(fs.readFileSync(ContractJsonPath, 'utf8'));
-    
-    switch (args[0]) {
+
+    switch (cmd) {
        case 'abi':
        default:
            abi(contract);
            break;
        case 'addr':
-           contract_address(contract, args[1]);
+           contract_address(contract, network_id);
            break;
     }
 }
@@ -26,8 +42,16 @@ function abi(contract) {
     console.log(JSON.stringify(contract.abi));
 }
 
-function contract_address(contract, networkId) {
-    console.log(contract.networks[networkId].address);
+function contract_address(contract, network_id) {
+    console.log(contract.networks[network_id].address);
+}
+
+function help() {
+    console.log("Run:");
+    console.log("node abi.js [cmd] [network_id] ([type])")
+    console.log("e.g.:")
+    console.log("node abi.js addr 50005 slim")
+    console.log("node abi.js abi 50005")
 }
 
 main();
